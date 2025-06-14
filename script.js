c873036206b3d2b478daa3f0f9f06c983a1f3d5e
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-06-14 23:49:03 +0700

    feat: validation input postal code

--- script.js
+++ script.js
@@ -2,6 +2,49 @@ const form = document.querySelector("#form-field");
 const inputEmail = document.querySelector("#email");
 const errorMessageEmail = document.querySelector(".error-message-email");
 const btnSubmit = document.querySelector("#btn-submit");
+const countryField = document.querySelector("#country");
+const postalCodeField = document.querySelector("#postal-code");
+const errorMessagePostalCode = document.querySelector(
+  ".error-message-postal-code"
+);
+
+function showErrorInputEmail() {
+  if (inputEmail.validity.valueMissing) {
+    errorMessageEmail.textContent = "You need enter an email address.";
+  } else if (inputEmail.validity.typeMismatch) {
+    errorMessageEmail.textContent =
+      "Entered value needs to be an email address.";
+  }
+}
+
+function checkPostalCode() {
+  let errorMessage = `You need to enter ${countryField.value} postal code.`;
+
+  const constraints = {
+    Indonesia: [
+      "^\\d{5}$",
+      "Indonesia postal codes must have exactly 5 digits: e.g. 10110",
+    ],
+    French: [
+      "^(F-)?\\d{5}$",
+      "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+    ],
+    German: [
+      "^(D-)?\\d{5}$",
+      "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+  };
+
+  const constraint = new RegExp(constraints[countryField.value][0], "");
+
+  if (postalCodeField.value === "") {
+    errorMessagePostalCode.textContent = errorMessage;
+  } else if (constraint.test(postalCodeField.value)) {
+    errorMessagePostalCode.textContent = "";
+  } else {
+    errorMessagePostalCode.textContent = constraints[countryField.value][1];
+  }
+}
 
 inputEmail.addEventListener("input", () => {
   console.log("input");
@@ -12,18 +55,14 @@ inputEmail.addEventListener("input", () => {
   }
 });
 
+countryField.addEventListener("change", checkPostalCode);
+postalCodeField.addEventListener("input", checkPostalCode);
+
 btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   if (!inputEmail.validity.valid) {
     showErrorInputEmail();
+    checkPostalCode();
   }
 });
 
-function showErrorInputEmail() {
-  if (inputEmail.validity.valueMissing) {
-    errorMessageEmail.textContent = "You need enter an email address.";
-  } else if (inputEmail.validity.typeMismatch) {
-    errorMessageEmail.textContent =
-      "Entered value needs to be an email address.";
-  }
-}
